a2ead32a0f4e5910c935d7fb41f221446205e010
Samama Shabbir <134776965+Samama225@users.noreply.github.com>
2025-04-04 15:41:49 +0500

    Add files via upload

---
+++ House_Price_Prediction_Web_App.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pickle
+import pandas as pd
+import streamlit as st
+
+
+# loading the saved model
+loaded_model = pickle.load(open(r'D:\ML_projects\model.pickle', 'rb'))
+
+
+
+
+
+
+st.title("ML Model Deployment")
+st.write("Enter the input features to get the prediction")
+
+
+# Collect user input
+feature1 = st.number_input("longitude")
+feature2 = st.number_input("latitute")
+feature3 = st.number_input("housing_median_age")
+feature4 = st.number_input("total_rooms")
+feature5 = st.number_input("total_bedrooms")
+feature6 = st.number_input("population")
+feature7 = st.number_input("households")
+feature8 = st.number_input("median_income")
+feature9 = st.number_input("median_house_value")
+
+# Prediction
+if st.button("Predict"):
+    prediction = loaded_model.predict([[feature1, feature2, feature3,feature4,feature5,feature6,feature7,feature8,feature9]])
+    st.success(f"Predicted Value: {prediction[0]}")
+
+    
+    
